3dc27d2f404c2b910b76a8b18e1a085197008ece
linux <dani173gonzalez@gmail.com>
2019-04-17 17:55:22 +0200

    first commit

---
+++ app/models/mhome.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace A4\App\Models;
+
+use A4\Sys\Model;
+
+
+class mHome extends Model{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getRol(){
+
+    }
+}
+
